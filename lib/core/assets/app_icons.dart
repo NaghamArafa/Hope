137b344aef3a1e33782220643b1833ae066576dc
NaghamArafa <naghamarafa33@gmail.com>
2025-02-12 00:52:01 +0200

    Basics

---
+++ lib/core/assets/app_icons.dart
@@ -0,0 +1,31 @@
+
+class AppIcons{
+
+  //Authentication
+  static const String userIcon = "assets/images/userIcon.png";
+  static const String emailIcon = "assets/images/emailIcon.png";
+  static const String passwordIcon = "assets/images/passwordIcon.png";
+  static const String phoneIcon = "assets/images/phoneIcon.png";
+  static const String calenderIcon = "assets/images/calenderIcon.png";
+  static const String google = "assets/images/google.png";
+
+  //Home
+  static const String homeIcon = "assets/images/homeIcon.png";
+  static const String homeFilled = "assets/images/homeFilled.png";
+
+  static const String addIcon = "assets/images/addIcon.png";
+  static const String addFilled = "assets/images/addFilled.png";
+
+  static const String scanIcon = "assets/images/scanIcon.png";
+  static const String scanFilled = "assets/images/scanFilled.png";
+
+  static const String awareIcon = "assets/images/awareIcon.png";
+  static const String awareFilled = "assets/images/awareFilled.png";
+
+  static const String menuIcon = "assets/images/menuIcon.png";
+  static const String menuFilled = "assets/images/menuFilled.png";
+
+
+
+
+}
